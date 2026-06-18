41d317e4220a2bc9a6b7a6900de0e5f3184424c4
luxudux <luxudux@gmail.com>
2026-06-18 16:26:12 -0600

    bump: actualizar a la version 1.0.3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 
 app.get('/', (req, res) => {
-  res.send(`<h1>🚀 Versión ${version} de la aplicación - Estable y Funcionando</h1>`);
+  res.send(`<h1>🚀 Versión ${version} de la aplicación - Estable y Funcionando desde GITHUB CONTAINER REGISTRY (GHCR)</h1>`);
   //res.send(`<h1>🚨 Versión ${version} - TODO ESTÁ ROTO EN PRODUCCIÓN</h1>`);
 });
 
