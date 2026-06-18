14b2c46163891431c53d861cfb04f3018468f756
luxudux <luxudux@gmail.com>
2026-06-18 14:49:19 -0600

    feat: lanzar version 1.0

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const version = 2.0;
+const version = 1.0;
 
 app.get('/', (req, res) => {
-  //res.send(`<h1>🚀 Versión ${version} de la aplicación - Estable y Funcionando</h1>`);
-  res.send(`<h1>🚨 Versión ${version} - TODO ESTÁ ROTO EN PRODUCCIÓN</h1>`);
+  res.send(`<h1>🚀 Versión ${version} de la aplicación - Estable y Funcionando</h1>`);
+  //res.send(`<h1>🚨 Versión ${version} - TODO ESTÁ ROTO EN PRODUCCIÓN</h1>`);
 });
 
 app.listen(PORT, () => {
